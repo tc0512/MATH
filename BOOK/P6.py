25c625608c1f546b9dc53ce01c9806e97710c47d
tc0512 <tancheng_0812@qq.com>
2026-05-24 04:11:13 +0000

    完成教材P6习题

---
+++ BOOK/P6.py
@@ -0,0 +1,5 @@
+A = 45
+DCA = 100
+B = DCA-A
+ACB = 180-DCA
+print(f"1、∠B={B}，∠ACB={ACB}")
--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "周测（5.3）"
 python EXAM/week53.py
+echo "单元测试（五） 分式与分式方程"
+python EXAM/U5.py

ba9edad110c7abddfcb3c8133b1b64f5581ef555
tc0512 <tancheng_0812@qq.com>
2026-05-30 21:53:14 +0800

    完成全卷解答

--- EXAM/2025shibei.py
+++ EXAM/2025shibei.py
@@ -6,7 +6,7 @@ from matplotlib.patches import Circle
 
 
 import numpy as np
-from sympy import Symbol, symbols, solve, factor, simplify, Rational, Eq
+from sympy import Symbol, symbols, solve, factor, simplify, Rational, sqrt, expand, solveset
 x, y = symbols("x y")
 ex1 = x/(x+y)
 x_new = 2*x
@@ -67,7 +67,7 @@ ang5 = 360-ang1-ang2-ang3-ang4
 AED = 180-ang5
 print(f"14、{AED}")
 
-print("第16题图片生成中...", end="")
+print("16、图片生成中...", end="")
 chinese_font = FontProperties(fname='/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc')
 x = [0, 4, 1, 0]
 y = [0, 0, 4, 0]
@@ -98,3 +98,106 @@ plt.title("第16题", fontproperties=chinese_font)
 plt.savefig("T16.png")
 plt.close()
 print("完成")
+
+print("17、")
+a, b, x = symbols("a b x")
+print(f"(1){factor(a*b**2-25*a)}")
+ine1 = (2*x+3>=-5)
+ine2 = (x-2<(x+4)/3)
+print(f"(2){solve([ine1, ine2], x)}")
+ex = (x-2)/(4-x)-2-x/(x-4)
+print(f"(3){solve(ex, x)[0]}")
+
+print("18、图片生成中...", end="")
+plt.plot([0, 0], [-4, 4], 'k-', linewidth=2)
+plt.plot([-4, 4], [0, 0], 'k-', linewidth=2)
+plt.text(0, 0, "O(B'')(B')", fontsize=15)
+plt.plot([-1, -3, -2, 0, -1], [-1, 0, 2, 1, -1], 'k-', linewidth=2)
+plt.text(-1, -1, 'A', fontsize=15)
+plt.text(-3, 0, 'B', fontsize=15)
+plt.text(-2, 2, 'C', fontsize=15)
+plt.text(0, 1, 'D', fontsize=15)
+plt.plot([2, 0, 1, 3, 2], [-1, 0, 2, 1, -1], 'gray', linewidth=2)
+plt.text(2, -1, "A''(C')", fontsize=15)
+plt.text(1, 2, "C''", fontsize=15)
+plt.text(3, 1, "D''", fontsize=15)
+plt.plot([-1, 0, 2, 1, -1], [-2, 0, -1, -3, -2], 'b-', linewidth=2)
+plt.text(-1, -2, "A'(-1, -2)", fontsize=15)
+plt.text(1, -3, "D'", fontsize=15)
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+plt.grid(True, linestyle="--", alpha=0.7)
+plt.gca().set_aspect('equal', adjustable='box')
+plt.title("第18题", fontproperties=chinese_font)
+plt.savefig("T18.png")
+plt.close()
+print("完成")
+
+a = sqrt(5)-1
+ex = (2+(1-a)/a)/((a**2+2*a+1)/a)
+print(f"19、{simplify(ex)}")
+print("21、")
+k1, b1, k2, b2 = symbols("k1 b1 k2 b2")
+x = [1, 2]
+f1 = [6000, 10500]
+f2 = [4800, 9600]
+ex1 = solve([f1[0]-k1*x[0]-b1, f1[1]-k1*x[1]-b1], [k1, b1])
+ex2 = solve([f2[0]-k2*x[0]-b2, f2[1]-k2*x[1]-b2], [k2, b2])
+x = Symbol("x")
+y1 = ex1[k1]*x+ex1[b1]
+y2 = ex2[k2]*x+ex2[b2]
+print(f"(1)y1={y1}\ny2={y2}")
+print(f"(2){str(solve(y1<y2))[:7]}")
+print("22、")
+a, b = symbols("a b")
+print(f"(1)(a+b)**2={expand((a+b)**2)}")
+simplify_res = str(expand((a+2*b)*(a+b)))
+No2 = simplify_res[-6]
+No3 = simplify_res[7]
+print(f"(2){No2};{No3}")
+print(f"(3){factor(a**2+4*a*b+3*b**2)}")
+
+print("(4)图片生成中...", end="")
+plt.plot([0, 5, 5, 0, 0], [0, 0, -4, -4, 0], 'k-', linewidth=2)
+plt.plot([2, 2], [0, -4], 'k-', linewidth=2)
+plt.plot([3, 3], [0, -4], 'k-', linewidth=2)
+plt.plot([4, 4], [0, -4], 'k-', linewidth=2)
+plt.plot([0, 5], [-2, -2], 'k-', linewidth=2)
+plt.plot([0, 5], [-3, -3], 'k-', linewidth=2)
+plt.text(1, -4, "a", fontsize=15)
+plt.text(2.5, -4, "b", fontsize=15)
+plt.text(3.5, -4, "b", fontsize=15)
+plt.text(4.5, -4, "b", fontsize=15)
+plt.text(5, -1, "a", fontsize=15)
+plt.text(5, -2.5, "b", fontsize=15)
+plt.text(5, -3.5, "b", fontsize=15)
+plt.text(1, 0.5, "a^2+5ab+6b^2=(a+2b)(a+3b)")
+plt.xlim(-1, 6)
+plt.ylim(-5, 1)
+plt.gca().set_aspect('equal', adjustable='box')
+plt.title("第22题第(4)问", fontproperties=chinese_font)
+plt.savefig("T22-4.png")
+plt.close()
+print("完成")
+
+print("24、")
+x = Symbol("x")
+sol = solve(1800/x-3000/(x+400), x)
+A = sol[0]
+B = A+400
+print(f"(1){A};{B}")
+print("25、")
+AD = 4*sqrt(3)
+AB = 1/2*AD
+BD = AB*sqrt(3)
+DG = 1/2*BD
+BF = AB
+S2 = BF*DG/2
+AF = BF
+AE = AF
+AH = AE*1/2
+EH = AH*sqrt(3)
+BH = EH
+BE = BH+EH
+S1 = BE*AH/2
+print(f"(2){S1};{S2}")

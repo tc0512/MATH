9a8d8371f9d12e16c2dc674d624ae94fc3bfc613
tc0512 <tancheng_0812@qq.com>
2026-05-31 12:24:49 +0800

    添加快速部署脚本

---
+++ clone_for_termux_on_android.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo -e "\033[32mVerifying environment...\033[0m"
+if command -v pkg >/dev/null 2>&1; then
+    echo "✓ Detected: Termux on Android"
+else
+    echo "::error::This script is not support this platform"
+fi
+echo -e "\033[32m[1/3] Install packages in isolated environment\033[0m"
+if command -v python >/dev/null 2>&1; then
+    echo "Python is already installed"
+else
+    echo "Python is not installed,so install it"
+    pkg update -y &>/dev/null
+    pkg install -y python 2>/dev/null 1>/dev/null
+fi
+if command -v git >/dev/null 2>&1; then
+    echo "git is already installed"
+else
+    echo "git is not installed,so install it"
+    pkg update -y &>/dev/null
+    pkg install -y git 2>/dev/null 1>/dev/null
+fi
+echo -e "\033[32m[2/3] Install runtime dependencies\033[0m"
+if pkg list-installed | grep -q "^matplotlib "; then
+    echo "matplotlib is already installed"
+else
+    echo "matplotlib is not installed,so install it"
+    pkg update -y &/dev/null
+    pkg install -y matplotlib 2>/dev/null 1>/dev/null
+fi
+if pip show sympy &>/dev/null; then
+    echo "sympy is already installed"
+else
+    echo "sympy is not installed,so install it"
+    pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ sympy
+fi
+echo -e "\033[32m[3/3] Clone repository\033[0m"
+rm -rf MATH
+git clone https://github.con/tc0512/MATH
